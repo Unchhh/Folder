2060d8ad1e68cf63ffb407f518436f0f00e22ed5
Unchhh <Killone@yandex.ru>
2019-12-21 17:20:44 -0600

    Сделаны задания 11 главы

---
+++ exercises/11_modules/task_11_2.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 11.2
+
+Создать функцию create_network_map, которая обрабатывает
+вывод команды show cdp neighbors из нескольких файлов и объединяет его в одну общую топологию.
+
+У функции должен быть один параметр filenames, который ожидает как аргумент список с именами файлов, в которых находится вывод команды show cdp neighbors.
+
+Функция должна возвращать словарь, который описывает соединения между устройствами.
+Структура словаря такая же, как в задании 11.1:
+    {('R4', 'Fa0/1'): ('R5', 'Fa0/1'),
+     ('R4', 'Fa0/2'): ('R6', 'Fa0/0')}
+
+
+Cгенерировать топологию, которая соответствует выводу из файлов:
+* sh_cdp_n_sw1.txt
+* sh_cdp_n_r1.txt
+* sh_cdp_n_r2.txt
+* sh_cdp_n_r3.txt
+
+В словаре, который возвращает функция create_network_map, не должно быть дублей.
+
+С помощью функции draw_topology из файла draw_network_graph.py нарисовать схему на основании топологии, полученной с помощью функции create_network_map.
+Результат должен выглядеть так же, как схема в файле task_11_2a_topology.svg
+
+
+При этом:
+* Расположение устройств на схеме может быть другим
+* Соединения должны соответствовать схеме
+
+Не копировать код функций parse_cdp_neighbors и draw_topology.
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+> Для выполнения этого задания, должен быть установлен graphviz:
+> apt-get install graphviz
+
+> И модуль python для работы с graphviz:
+> pip install graphviz
+
+'''
+#Решение
+from draw_network_graph import draw_topology
+def create_network_map (filenames):
+	ans,ans1={},{}
+	for files in filenames:
+		with open (files) as f:	
+			lis = f.read().split('\n')
+		lis=[x for x in lis if x!='']
+		loc_name = lis[0][:lis[0].find('>')].strip()
+		for i in range(4,len(lis)):
+			lis1=lis[i].split()
+			ans[(loc_name,lis1[1]+lis1[2])]=(lis1[0],lis1[-2]+lis1[-1])	
+	for keys in ans.keys():
+		if keys not in ans1.values():
+			ans1[keys]=ans[keys]
+	return ans1
+	
+if __name__=='__main__':
+	draw_topology(create_network_map(['sh_cdp_n_sw1.txt','sh_cdp_n_r1.txt','sh_cdp_n_r2.txt','sh_cdp_n_r3.txt']),'topol')
+
+	
+
